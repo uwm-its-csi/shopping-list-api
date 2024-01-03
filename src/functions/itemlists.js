a63b44faf684c9602aedede8bb2639ce17838be5
Robert Beerman <rbeerma@uw.edu>
2024-01-03 07:49:53 -0800

    Implement conditional get in itemlists endpoing

--- src/functions/itemlists.js
+++ src/functions/itemlists.js
@@ -1,11 +1,8 @@
 const { app } = require('@azure/functions');
 
-//require('dotenv').config();
-
 const { MongoClient } = require('mongodb');
 
 const uri = process.env.COSMOS_CONNECTION_STRING;
-
 const client = new MongoClient(uri,
     {
         tlsAllowInvalidCertificates: true
@@ -16,6 +13,8 @@ app.http('itemlists', {
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
+        const store = request.query.get('store') || null;
+
         const options = {
             projection: { _id: 0, store: 1, items: 1 },
         };
@@ -32,13 +31,35 @@ app.http('itemlists', {
             context.log(err.message);
         }
 
-        try {
-            const lists = await collection.find({ }, options).toArray();
-            result = JSON.stringify(lists);
-        } catch (err) {
-            context.log(err.message);
-        } finally {
-            await client.close();
+        if (store) {
+            context.log(`store param: ${store}`);
+
+            try {
+                const listDoc = await collection.findOne({
+                    store: store,
+                });
+
+                if (listDoc) {
+                    const list = listDoc.items;
+                    console.dir(`Found list: ${JSON.stringify(list)}`);
+                    result = JSON.stringify(list);
+                } else {
+                    result = '[]';
+                }
+            } catch (err) {
+                context.log(err.message);
+            } finally {
+                await client.close();
+            }
+        } else {
+            try {
+                const lists = await collection.find({ }, options).toArray();
+                result = JSON.stringify(lists);
+            } catch (err) {
+                context.log(err.message);
+            } finally {
+                await client.close();
+            }
         }
 
         return {
