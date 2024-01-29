d1f5c556f45f61f86a3626a305eb3207dc153c8a
Robert Beerman <rbeerma@uw.edu>
2024-01-28 20:58:14 -0800

    Implement itemlists POST

--- src/functions/itemlists.js
+++ src/functions/itemlists.js
@@ -1,73 +1,134 @@
 const { app } = require('@azure/functions');
-
 const { MongoClient } = require('mongodb');
 
 const uri = process.env.COSMOS_CONNECTION_STRING;
 const client = new MongoClient(uri);
-/* const client = new MongoClient(uri,
-    {
-        tlsAllowInvalidCertificates: true
+
+async function handleGet(req, context) {
+    const store = req.query.get('store') || null;
+
+    const options = {
+        projection: { _id: 0, store: 1, items: 1 },
+    };
+
+    let collection;
+    let result;
+
+    try {
+        await client.connect();
+        collection = client.db('shoppinglist').collection('itemlists');
+    } catch (err) {
+        context.log(err.message);
     }
-); */
 
-app.http('itemlists', {
-    methods: ['GET'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        const store = request.query.get('store') || null;
-
-        const options = {
-            projection: { _id: 0, store: 1, items: 1 },
-        };
-
-        let db;
-        let collection;
-        let result;
+    if (store) {
+        context.log(`store param: ${store}`);
 
         try {
-            await client.connect();
-            db = client.db('shoppinglist');
-            collection = db.collection('itemlists');
+            const listDoc = await collection.findOne({
+                store: store,
+            });
+
+            if (listDoc) {
+                const list = listDoc.items;
+                context.log(`Found list: ${JSON.stringify(list)}`);
+                result = JSON.stringify(list);
+            } else {
+                result = '[]';
+            }
         } catch (err) {
             context.log(err.message);
+        } finally {
+            await client.close();
         }
+    } else {
+        try {
+            const lists = await collection.find({ }, options).toArray();
+            result = JSON.stringify(lists);
+        } catch (err) {
+            context.log(err.message);
+        } finally {
+            await client.close();
+        }
+    }
 
-        if (store) {
-            context.log(`store param: ${store}`);
+    return {
+        body: result,
+        headers: {
+            'content-type': 'application/json'
+        }
+    };
+}
 
-            try {
-                const listDoc = await collection.findOne({
-                    store: store,
-                });
+async function handlePost(reqBody, context) {
+    let result;
 
-                if (listDoc) {
-                    const list = listDoc.items;
-                    console.dir(`Found list: ${JSON.stringify(list)}`);
-                    result = JSON.stringify(list);
-                } else {
-                    result = '[]';
+    context.log(`POST received: ${JSON.stringify(reqBody)}`);
+
+    const filter = { store: reqBody.store };
+    const options = {
+        upsert: true,
+        returnDocument: 'after',
+    };
+    
+    const update = {
+        $addToSet: {
+            items: {
+                'name': reqBody.item.name
+            }
+        }
+    }
+
+    try {
+        await client.connect();
+        const upsertResult = await client.db('shoppinglist').collection('itemlists').findOneAndUpdate(filter, update, options);
+        result = JSON.stringify(upsertResult);
+
+        context.log(result);
+    } catch (err) {
+        context.log(err.message);
+    } finally {
+        await client.close();
+    }
+
+    return {
+        body: result,
+        headers: {
+            'content-type': 'applicatoin/json'
+        }
+    };
+}
+
+app.http('itemlists', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        switch (request.method) {
+            case 'GET':
+                return handleGet(request, context);
+                break;
+            case 'POST': {
+                const reqBody = await request.json()
+                    .catch((err) => {
+                        context.log(err);
+                        return {
+                            jsonBody: 'Missing request body',
+                            status: 400
+                        }
+                    });
+
+                if (!reqBody || !reqBody.store) {
+                    return {
+                        jsonBody: 'Missing store property',
+                        status: 400,
+                    }
                 }
-            } catch (err) {
-                context.log(err.message);
-            } finally {
-                await client.close();
+            
+                return handlePost(reqBody, context);
+                break;
             }
-        } else {
-            try {
-                const lists = await collection.find({ }, options).toArray();
-                result = JSON.stringify(lists);
-            } catch (err) {
-                context.log(err.message);
-            } finally {
-                await client.close();
-            }
+            default:
+                console.log('Unrecognized request method!');
         }
-
-        return {
-            body: result,
-            headers: {
-                'content-type': 'application/json'
-            }
-        };
     }
 });
